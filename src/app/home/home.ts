196f4b19c70dd9b51f3316df59b968d85dc247ae
Kostiantyn Rakilov <rakilovkg@gmail.com>
2026-09-11 16:33:33 +0300

    Added HousingLocationInfo interface

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HousingLocation } from '../housing-location/housing-location';
+import { HousingLocationInfo } from '../housinglocation';
 
 @Component({
   imports: [HousingLocation],
@@ -19,4 +20,16 @@ import { HousingLocation } from '../housing-location/housing-location';
 `,
 })
 export class Home {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocationInfo: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
